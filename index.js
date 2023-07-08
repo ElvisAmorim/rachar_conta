9b7f634eeccb2ef8b7b95cb6b7a7ac0dfea88dc4
ElvisAmorim <93032392+ElvisAmorim@users.noreply.github.com>
2023-07-08 19:01:54 -0300

    projeto finalizado em js

---
+++ index.js
@@ -0,0 +1,36 @@
+/* funcao para fixar duas casas decimais */
+
+function formatVl(value){
+    value = value.toFixed(2)
+    return 'R$ ' + value
+}
+
+
+function update(){
+
+    /* variaveis associadas ao html */
+    let conta = Number(document.getElementById('vlConta').value)
+    let perc = document.getElementById('percInput').value
+    let div = document.getElementById('divInput').value
+  
+    /* Operacoes matematicas */
+    let gorjVlr = conta * (perc / 100)
+    let contaTotal = conta + gorjVlr
+    let contaDiv = contaTotal / div
+ 
+    /* % da gorjeta */
+    document.getElementById('tipPercent').innerHTML = perc + ' %'
+  
+    /* Valor da gorjeta */
+    document.getElementById('gorjVl').innerHTML = formatVl(gorjVlr)
+
+    /* Valor da conta mais valor da gorjeta */
+    document.getElementById('totalComGorj').innerHTML = formatVl(contaTotal)
+
+    /* Quantas divisoes */
+    document.getElementById('divGorj').innerHTML = div
+
+    /* Valor por pessoa */
+    document.getElementById('divPessoa').innerHTML = formatVl(contaDiv)
+    
+}
